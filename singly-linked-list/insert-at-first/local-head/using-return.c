6b3c0278ccdb58d9c72fe3ffbf80325e032dd8ee
Avik Agarwala <avikagarwala@outlook.com>
2023-10-11 11:57:37 +0530

    singly linked list local head implementation 1

---
+++ singly-linked-list/insert-at-first/local-head/using-return.c
@@ -0,0 +1,47 @@
+// singly linked list local head implementation using return
+
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
+
+Node *insertAtFirst(Node *, int data);
+void showList(Node *);
+
+int main()
+{
+    Node *head = NULL;
+
+    head = insertAtFirst(head, 3);
+    head = insertAtFirst(head, 2);
+    head = insertAtFirst(head, 1);
+
+    showList(head);
+    return 0;
+}
+
+Node *insertAtFirst(Node *head, int data)
+{
+    Node *nw = malloc(sizeof(Node));
+    nw->data = data;
+
+    nw->next = head;
+    head = nw;
+
+    return head;
+}
+
+void showList(Node *head)
+{
+    while (NULL != head)
+    {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+
+    printf("\n");
+}
