27087c158e4284940654134c3402c43d5ac3b3ab
Avik Agarwala <avikagarwala@outlook.com>
2023-10-11 11:48:34 +0530

    Add linked list global head insert at first

---
+++ singly-linked-list/insert-at-first/global-head.c
@@ -0,0 +1,47 @@
+// singly linked list global head implementation
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}Node;
+
+Node *head = NULL; // declaring the head pointer
+
+void insertAtBeginning(int);
+void display();
+
+int main(void)
+{
+    insertAtBeginning(30);
+    insertAtBeginning(20);
+    insertAtBeginning(10);
+
+    display();
+
+    return 0;
+}
+
+void insertAtBeginning(int data)
+{
+   Node *temp = malloc(sizeof (Node));
+   temp->data = data;
+   temp->next = head;
+   head = temp;
+}
+
+void display()
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("\n");
+}
