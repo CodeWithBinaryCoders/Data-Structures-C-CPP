fbc0100f822d2281cfd65497781f401b8da93b7c
Avik Agarwala <avikagarwala@outlook.com>
2023-11-22 14:53:52 +0530

    Iterative reversal of linked list

---
+++ singly-linked-list/reverse-list/iterative-reversal.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next = nullptr;
+
+    Node(int data)
+    {
+        this->data = data;
+    }
+};
+
+class LinkedList
+{
+    Node *head = nullptr;
+
+public:
+    void insertAtBeginning(int data)
+    {
+        Node *newNode = new Node(data);
+
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void reverseList()
+    {
+        Node *currentNode, *previousNode, *nextNode;
+
+        currentNode = head;
+        previousNode = nullptr;
+
+        while (currentNode) // while (NULL != currentNode)
+        {
+            nextNode = currentNode->next;
+            currentNode->next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        head = previousNode;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+
+        while (temp) // while (NULL != temp)
+        {
+            cout << temp->data << ' ';
+            temp = temp->next;
+        }
+
+        cout << endl;
+    }
+};
+
+int main()
+{
+    LinkedList l;
+
+    l.insertAtBeginning(3); // 3
+    l.insertAtBeginning(2); // 2 3
+    l.insertAtBeginning(1); // 1 2 3
+
+    cout << "Origial list:" << endl;
+
+    l.display(); // 1 2 3
+
+    l.reverseList();
+
+    cout << endl << "Reversed list:" << endl;
+    l.display(); // 3 2 1
+
+    return 0;
+}
