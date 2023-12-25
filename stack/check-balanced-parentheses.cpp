3536d2b754efc377bfa0e94fbb107f90dd5705ee
Alik Agarwala <alikagarwala@outlook.com>
2023-12-26 00:23:18 +0530

    add check-balanced-parentheses.cpp

---
+++ stack/check-balanced-parentheses.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool checkValidPair(char opening, char closing)
+{
+    if (opening == '(' && closing == ')')
+        return true;
+    return false;
+}
+
+bool checkBalancedBrackets(string expression)
+{
+    stack<char> s;
+
+    for (char ch : expression)
+        if (ch == '(')
+            s.push(ch);
+        else if (ch == ')')
+        {
+            if (s.empty() || !checkValidPair(s.top(), ch))
+                return false;
+            else
+                s.pop();
+        }
+
+    return s.empty() ? true : false;
+}
+
+int main()
+{
+    string expression;
+    getline(cin, expression);
+
+    if (checkBalancedBrackets(expression))
+        cout << "Balanced" << endl;
+    else
+        cout << "Not balanced" << endl;
+
+    return 0;
+}
